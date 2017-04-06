e8c2b9538698fb41d964d2ebd9b6feb4848f8dd2
Andrew Hoglund <ahoglund@tunecore.com>
2017-04-06 14:15:17 -0400

    add cleaning fee to drew

--- airbnb_calc.rb
+++ airbnb_calc.rb
@@ -8,7 +8,7 @@ def airbnb_calc(total_money, total_days)
   cleaning_fee   = 95
   profit         = ((total_money - moms_fee) - drews_fee) - cleaning_fee
   moms_takehome  = moms_fee + (profit / 2)
-  drews_takehome = drews_fee + (profit / 2)
+  drews_takehome = drews_fee + (profit / 2) + cleaning_fee
 
   puts "Total Profit: $#{profit}"
   puts "Mom Earned: $#{moms_takehome}"
