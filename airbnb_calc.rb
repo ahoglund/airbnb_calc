bed29f55d4519ec623e5170f031aa4834e5ebc97
Andrew Hoglund <ahoglund@tunecore.com>
2017-04-06 13:14:19 -0400

    another

---
+++ airbnb_calc.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+def airbnb_calc(total_money, total_days)
+  total_money    = total_money.to_i
+  total_days     = total_days.to_i
+  moms_fee       = total_days * 50
+  drews_fee      = total_money / 10
+  cleaning_fee   = 95
+  profit         = ((total_money - moms_fee) - drews_fee) - cleaning_fee
+  moms_takehome  = moms_fee + (profit / 2)
+  drews_takehome = drews_fee + (profit / 2)
+
+  puts "Total Profit: $#{profit}"
+  puts "Mom Earned: $#{moms_takehome}"
+  puts "Drew Earned: $#{drews_takehome}"
+end
+
+airbnb_calc(ARGV[0], ARGV[1])
